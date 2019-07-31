50ba750f9f1b9cc33d83595256a7b637adc02d02
Jigar Gosar <jigar.gosar@gmail.com>
2019-07-31 14:42:28 +0530

    http sync mock server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+import { mergeRight } from 'ramda'
+
+const nanoid = require('nanoid')
+const faker = require('faker')
 const logger = require('koa-logger')
 const router = require('koa-router')()
 const koaBody = require('koa-body')
@@ -17,6 +21,9 @@ app.use(koaBody())
 
 router.get('/', hello)
 router.get('/hello', hello)
+router.get('/all', async ctx => {
+  ctx.body = { todoList: [] }
+})
 // .get('/post/new', add)
 // .get('/post/:id', show)
 // .post('/post', create);
@@ -26,7 +33,7 @@ app.use(router.routes())
 
 async function hello(ctx) {
   // await ctx.render('list', { posts: posts })
-  ctx.body = {msg:"ECHO", payload:"payload10"}
+  ctx.body = { msg: 'ECHO', payload: 'payload10' }
 }
 
 
@@ -52,5 +59,35 @@ async function hello(ctx) {
 // }
 
 
-
 if (!module.parent) app.listen(3000)
+
+
+function createFakeTask(overrides) {
+  const now = Date.now()
+  return mergeRight({
+    id: nanoid(),
+    title: faker.hacker.phrase(),
+    sortIdx: 0,
+    projectId: '',
+    isDone: faker.random.boolean(),
+    createdAt: now,
+    modifiedAt: now,
+  }, overrides)
+}
+
+function createFakeProject(overrides) {
+  const now = Date.now()
+
+  return mergeRight({
+    id: nanoid(),
+    title:
+      faker.hacker.verb() +
+      ' ' +
+      faker.hacker.ingverb() +
+      ' ' +
+      faker.hacker.noun(),
+    sortIdx: 0,
+    createdAt: now,
+    modifiedAt: now,
+  }, overrides)
+}
